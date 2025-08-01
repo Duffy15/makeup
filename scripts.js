2a5e33355877e98d5dc9827d529615c0e10df45b
Duffy15 <duffy5822@gmail.com>
2025-08-01 22:28:25 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Slider Functionality
-    const sliderContainer = document.querySelector('.slider-container');
-    const slides = document.querySelectorAll('.slide');
-    let currentIndex = 0;
-    const slideWidth = slides[0].offsetWidth;
-    const intervalTime = 3000; // 3 seconds
-
-    function moveSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        const offset = -currentIndex * slideWidth;
-        sliderContainer.style.transform = `translateX(${offset}px)`;
-    }
-
-    setInterval(moveSlide, intervalTime);
-
-    // Reset slider position if window resized
-    window.addEventListener('resize', () => {
-        const newSlideWidth = slides[0].offsetWidth;
-        const offset = -currentIndex * newSlideWidth;
-        sliderContainer.style.transform = `translateX(${offset}px)`;
-    });
-
+    
     // Lightbox Gallery Functionality for Portfolio Page
     const galleryLinks = document.querySelectorAll('.gallery a');
     console.log('Gallery links found:', galleryLinks); // Debug: Check if links are detected
